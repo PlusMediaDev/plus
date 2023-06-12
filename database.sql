a4c9f3394a9b2b42f7e607be0aecc76ebd068b2d
Kobi Jones <kobijonesmail@gmail.com>
2023-06-12 15:26:28 -0500

    Add database setup sql

--- database.sql
+++ database.sql
@@ -1,10 +1,38 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "users";
--- Otherwise you will have errors!
 CREATE TABLE "users" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+	"id" SERIAL PRIMARY KEY,
+	"email" VARCHAR(254) NOT NULL UNIQUE,
+	"password" VARCHAR(1000) NOT NULL
+);
+
+CREATE TABLE "uploads_for_rating" (
+	"id" SERIAL PRIMARY KEY,
+	"content_url" TEXT NOT NULL,
+	"total_ratings" INT NOT NULL DEFAULT 0,
+	"uploaded_at" TIMESTAMP NOT NULL,
+	"user_id" INT NOT NULL REFERENCES "users" ON DELETE CASCADE
+);
+
+CREATE TABLE "ratings" (
+	"id" SERIAL PRIMARY KEY,
+	"rating" INT NOT NULL,
+	"upload_id" INT NOT NULL REFERENCES "uploads_for_rating" ON DELETE CASCADE,
+	"user_id" INT NOT NULL REFERENCES "users" ON DELETE CASCADE,
+	UNIQUE ("upload_id", "user_id")
+);
+
+CREATE TABLE "uploads_for_matching" (
+	"id" SERIAL PRIMARY KEY,
+	"average_rating" DOUBLE NOT NULL,
+	"total_matchings" INT NOT NULL DEFAULT 0,
+	"last_matched_at" TIMESTAMP,
+	"uploaded_at" TIMESTAMP,
+	"user_id" INT NOT NULL REFERENCES "users" ON DELETE CASCADE
+);
+
+CREATE TABLE "matches" (
+	"id" SERIAL PRIMARY KEY,
+	"upload_1_id" INT NOT NULL REFERENCES "uploads_for_matching" ON DELETE CASCADE,
+	"upload_2_id" INT NOT NULL REFERENCES "uploads_for_matching" ON DELETE CASCADE,
+	UNIQUE ("upload_1_id", "upload_2_id"),
+	CHECK ("upload_1_id" != "upload_2_id")
 );
