99a71e5362762933b8176fb555a611a6d6a1417e
Kobi Jones <kobijonesmail@gmail.com>
2023-06-13 09:46:14 -0500

    Fix react not being able to find proxy server

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,13 +1,13 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-const port = process.env.SERVER_PORT || 5000;
+const port = Number(process.env.SERVER_PORT) || 5000;
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
       target: `http://localhost:${port}`,
       changeOrigin: true,
-    }),
+    })
   );
-}
+};
