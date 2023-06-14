3fe15b4dd72300e000143ded45747ccb6e8d531b
Japheth Yang <japheth720@gmail.com>
2023-06-14 10:44:11 -0500

    minor css and component changes

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -9,36 +9,22 @@ function Nav() {
 
   return (
     <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
+      <Link to="/landingPage">
+        <h2 className="nav-title">✚</h2>
       </Link>
       <div>
-        {/* If no user is logged in, show these links */}
-        {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        )}
 
         {/* If a user is logged in, show these links */}
         {user.id && (
           <>
-            <Link className="navLink" to="/user">
-              Home
-            </Link>
-
-            <Link className="navLink" to="/info">
-              Info Page
+            <Link className="navLink" to="/accountPage">
+              ☃️
             </Link>
 
             <LogOutButton className="navLink" />
           </>
         )}
 
-        <Link className="navLink" to="/about">
-          About
-        </Link>
       </div>
     </div>
   );
