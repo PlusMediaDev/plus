477918a4d7c738f6b4bc3069652054a944000c0d
Ilyas Mohamed <zswaleh@ymail.com>
2023-06-15 16:01:38 -0500

    show case now works with the updates, accounts show all tokens won

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
 import memeStorage from './showcase.reducer';
-
+import tokenWon from './tokenswon.reducer';
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
 // This is imported in index.js as rootSaga
@@ -12,7 +12,8 @@ import memeStorage from './showcase.reducer';
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
-  memeStorage
+  memeStorage,
+  tokenWon
 });
 
 export default rootReducer;
